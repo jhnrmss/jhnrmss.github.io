c47a8db8c7934eaf357dab946ef749150bada00a
John Romie Reyes <john.reyes@hoolisoftware.com>
2024-02-22 08:45:04 +0800

    fix/fixing project page

--- src/api/Personal/index.ts
+++ src/api/Personal/index.ts
@@ -146,7 +146,7 @@ export const dummyData = [
   {
     title: "Sample Project 3",
     description:
-      " Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+    " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
     tech: ["ReactJS", "Tailwind", "TypeScript"],
   },
 ];
--- src/pages/Project/Cards/index.tsx
+++ src/pages/Project/Cards/index.tsx
@@ -7,7 +7,7 @@ function ProjectCards() {
   return (
     <div className="grid grid-cols-1 gap-10">
       {dummyData.map((data) => (
-        <div key={data.title} className="relative w-full">
+        <div key={data.title} className="relative h-full w-full">
           <div
             className={cn(
               "flex  bg-gray-600 max-h-[24rem] md:h-[18rem] md:w-[24rem] md:min-w-[18rem] rounded-md"
@@ -21,7 +21,7 @@ function ProjectCards() {
           </div>
           <div
             className={cn(
-              "p-6 space-y-6 md:space-y-2  absolute top-0 right-0 text-left md:text-right w-full md:w-3/4 flex flex-col justify-center content-center rounded-md "
+              "p-6 space-y-2 h-full absolute top-0 right-0 text-left md:text-right  w-full md:w-3/4 flex flex-col justify-center content-center rounded-md "
             )}
           >
             <div
@@ -29,15 +29,15 @@ function ProjectCards() {
                 "md:border-r-2 md:border-r-green-700 space-y-1 md:px-2"
               )}
             >
-              <p className="text-xs mt-6 md:mt-0 text-green-400 md:text-green-700 tracking-wider font-medium">
+              <p className="text-[min(1.5vw,0.75rem)] text-green-400 md:text-green-700 tracking-wider font-medium">
                 Featured Project
               </p>
-              <p className="text-base text-gray-300 md:text-gray-700 tracking-wider font-medium">
+              <p className="text-[min(2.5vw,1rem)] text-gray-300 md:text-gray-700 tracking-wider font-medium">
                 {data.title}
               </p>
             </div>
-            <div className="w-full md:p-3 md:bg-slate-100 shadow-md text-gray-300 md:text-gray-700 rounded-md ">
-              <p>{data.description}</p>
+            <div className="w-full  md:p-3 md:bg-slate-100 shadow-md text-gray-300 md:text-gray-700 rounded-md">
+              <p className="text-[min(2vw,1rem)]">{data.description}</p>
             </div>
             <div className="space-y-4 md:space-y-2">
               <div
@@ -46,7 +46,7 @@ function ProjectCards() {
                 {data.tech.map((techs) => (
                   <p
                     key={techs}
-                    className="text-sm text-gray-300 md:text-gray-700 tracking-wider font-medium"
+                    className="text-[min(2vw,0.85rem)] text-gray-300 md:text-gray-700 tracking-wider font-medium"
                   >
                     {techs}
                   </p>
@@ -55,8 +55,8 @@ function ProjectCards() {
               <div
                 className={cn("flex justify-start md:justify-end space-x-3")}
               >
-                <TbBrandGithub className="h-5 w-5 text-gray-300 md:text-gray-700" />
-                <FaExternalLinkAlt className="h-5 w-5 text-gray-300 md:text-gray-700" />
+                <TbBrandGithub className="h-[min(2vw,1rem)] w-[min(2vw,1rem)] text-gray-300 md:text-gray-700" />
+                <FaExternalLinkAlt className="h-[min(2vw,1rem)] w-[min(2vw,1rem)] text-gray-300 md:text-gray-700" />
               </div>
             </div>
           </div>
